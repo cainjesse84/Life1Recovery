c8930899f7ad109b9a1160579d61ca877f9c755e
cainjesse84 <111395502+cainjesse84@users.noreply.github.com>
2025-07-01 14:16:52 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from threading import Thread
+import telebot
+
+# Keep-alive web server for Render
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
+# Telegram
